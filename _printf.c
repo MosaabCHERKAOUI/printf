4835f72d81d7b8aa60e63fa5e74868d133994d90
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-06-26 21:21:37 +0100

    tesst

--- _printf.c
+++ _printf.c
@@ -3,7 +3,14 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int i, sum;
 
 	va_start(args, format);
+
+	for (i = 0; i < 2; i++)
+	{
+		sum = va_arg(args, int);
+	}
 	va_end(args);
+	return (sum);
 }
