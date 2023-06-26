978438b396336f5f90ab399c186b724c441d643a
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-06-26 23:38:47 +0100

    donnee

--- _printf.c
+++ _printf.c
@@ -1,16 +1,55 @@
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * _printf - my printf
+ * @format: strings
+ * @...: s, c, %
+ * Return: 0
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i, sum;
+	int i, len = 0, fix = 0, lenn = 0;
+	char *s, c, modu, bracket = ']', *newline = "\n";
 
 	va_start(args, format);
+	while (format[len])
+		len++;
 
-	for (i = 0; i < 2; i++)
+
+	for (i = 0; i < len; i++)
+		if (format[i] == '%')
+		{
+			fix = i;
+		}
+
+	write(1, format, fix);
+
+	for (i = 0; i < len; i++)
 	{
-		sum = va_arg(args, int);
+		if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			c = va_arg(args, int);
+			write(1, &c, 1);
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			s = va_arg(args, char*);
+			lenn = strlen(s);
+			write(1, s, lenn);
+		}
+		if (format[i] == '%' && format[i + 1] == '%')
+		{
+			write(1, &modu, 1);
+		}
 	}
+	write(1, &bracket, 1);
+	write(1, newline, 1);
 	va_end(args);
-	return (sum);
+
+	return (0);
 }
