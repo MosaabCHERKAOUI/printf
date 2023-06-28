3387f33aa51b6c36921e6996da7c05455b804428
Ahdad0 <Abderrahimahdadabdo@gmail.com>
2023-06-28 02:33:15 +0100

    check

--- _printf.c
+++ _printf.c
@@ -16,9 +16,15 @@ int _printf(const char *format, ...)
 	char *s;
 
 	va_start(args, format);
+
 	len = strlen(format);
+
 	if (format == NULL || (format[0] == '%' && format[1] == NULL))
 		return (-1);
+
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
 	for (i = 0; i < len; i++)
 	{
 		if (format[i] != '%')
